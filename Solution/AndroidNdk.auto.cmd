760331c590c061faabaad2d069148e1bfcc26edd
unicornum.verum <unicornum.verum@gmail.com>
2024-12-25 22:33:12 +0400

    [Covellite++] Исправления с учетом изменений Common.Externals.

---
+++ Solution/AndroidNdk.auto.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+rem !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+rem !                This is an automatically generated file                 !
+rem !                    all changes will be overwritten                     !
+rem !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+set ANDROID_NDK_ROOT="E:\Android\sdk\25.custom"
