f46394bcba05aa1044c859eeaf852f78e861290f
unicornum.verum <unicornum.verum@gmail.com>
2025-01-03 13:03:38 +0400

    [Covellite++] Перенастройка сборки SDK с учетом внешнего расположения Alicorn SDK.

---
+++ Solution/Solution/SDK/AlicornSdkPath.auto.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+rem !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+rem !                This is an automatically generated file                 !
+rem !                    all changes will be overwritten                     !
+rem !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+set AlicornSdkPath="P:\sdk\Alicorn\v3.10.1.0\"
