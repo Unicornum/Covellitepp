1d341370438c468c113e4740e01e6dc2cd54eee4
unicornum.verum <unicornum.verum@gmail.com>
2025-04-08 18:32:07 +0400

    [Covellite++] Информация о работе над решением.

--- Solution/Solution/SDK/AlicornSdkPath.auto.cmd
+++ Solution/Solution/SDK/AlicornSdkPath.auto.cmd
@@ -5,4 +5,4 @@ rem !                This is an automatically generated file                 !
 rem !                    all changes will be overwritten                     !
 rem !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-set AlicornSdkPath="P:\sdk\Alicorn\v3.10.1.0\"
+set AlicornSdkPath="P:\sdk\Alicorn\v3.10.1.0-Vs2022\"
